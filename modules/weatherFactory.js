f98ee3843454a31a974aae2085192d0528b9e687
jlbrd <jlbiord@gmail.com>
2017-11-04 16:06:06 +0100

    Update weather factory

--- modules/weatherFactory.js
+++ modules/weatherFactory.js
@@ -2,14 +2,23 @@ angular
     .module('chauffage')
     .factory('Weather', ['$http', function ($http) {
         var getCurrentWeather = function () {
-            var urlOpenweathermap = "http://api.openweathermap.org/data/2.5/weather?q=XXX,fr&appid=APPID&lang=fr&units=metric";
-            return $http.get(urlOpenweathermap)
+            var urlOpenweathermap = ""; // Enter here the api.openweathermap.org URL
+            var urlWunderground = ""; // Enter here the api.wunderground.com URL
+            var urlWeather = urlWunderground;
+            return $http.get(urlWeather)
                 .then(function (response) {
                     var _data = response.data;
                     var weather = {};
-                    weather.temp = _data.main.temp;
-                    weather.icon = _data.weather[0].icon;
-                    weather.description = _data.weather[0].description;
+                    if (urlWeather === urlOpenweathermap) {
+                        weather.temp = _data.main.temp;
+                        weather.icon = "http://openweathermap.org/img/w/" + _data.weather[0].icon + ".png";                        
+                        weather.description = _data.weather[0].description;
+                    }
+                    else if (urlWeather === urlWunderground) {
+                        weather.temp = _data.current_observation.temp_c;
+                        weather.icon = _data.current_observation.icon_url;
+                        weather.description = _data.current_observation.weather;
+                    }
                     return weather;
                 });
         }
